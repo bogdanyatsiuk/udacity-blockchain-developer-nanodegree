dda1bd98dd8a1b590a3aeb3d3845540d1866876c
Bohdan Yatsiuk <emailbogdans@gmail.com>
2023-05-28 20:37:39 +0300

    Exercise hasing: hash function corrected

--- exercises/01_hashing/app.js
+++ exercises/01_hashing/app.js
@@ -1,20 +1,17 @@
 var SHA256 = require("crypto-js/sha256");
 
-const data1 = "Blockchain Rock!";
-const dataObject = {
+var data1 = "Blockchain Rock!";
+var dataObject = {
     id: 1,
     body: "With Object Works too",
     time: new Date().getTime().toString().slice(0,-3)
 };
 
 function generateHash(obj) {
-  console.log(obj);
-  return SHA256(obj);
+  var str = JSON.stringify(obj);
+  return SHA256(str);
 }
 
 console.log(`SHA256 Hash: ${generateHash(data1)}`);
 console.log("************************************");
 console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
-
-console.log(`SHA256 Hash: ${generateHash("Object")}`);
-
